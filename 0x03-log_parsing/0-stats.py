82c0e2983bba608844b28be61b7498cda288559e
Eugene <140322568+yoojen@users.noreply.github.com>
2023-12-21 18:16:17 +0200

    Update 0-stats.py

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -13,7 +13,7 @@ def print_msg(dict_sc, total_file_size):
         Nothing
     """
 
-    print(f"File size: {total_file_size}")
+    print("File size: {}".format(total_file_size))
     for key, val in sorted(dict_sc.items()):
         if val != 0:
             print("{}: {}".format(key, val))
