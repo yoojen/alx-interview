912689721fbaf77992ad8c79c95f89f71d37eade
yoojen <eugeneemma7@gmail.com>
2024-01-11 22:42:27 +0200

    made executable

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -6,12 +6,13 @@ import sys
 if len(sys.argv) < 2 or len(sys.argv) > 2:
     print("Usage: nqueens N")
     exit(1)
-if int(sys.argv[1]) < 4:
-    print("N must be at least 4")
-    exit(1)
 if not sys.argv[1].isdigit():
     print("N must be a number")
     exit(1)
+if int(sys.argv[1]) < 4:
+    print("N must be at least 4")
+    exit(1)
+
 n = int(sys.argv[1])
 
 
