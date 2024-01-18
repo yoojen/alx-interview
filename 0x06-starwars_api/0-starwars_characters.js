e8eba5907defb4210977ea83ddac668bc3aa9428
Eugene <140322568+yoojen@users.noreply.github.com>
2024-01-18 21:58:01 +0200

    Update 0-starwars_characters.js

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,15 +1,14 @@
 #!/usr/bin/node
 const request = require("request");
 
-const base_url = `https://swapi-api.alx-tools.com/api/films/${process.argv[2]}`;
-request(base_url, (error, response, body) => {
+const base_url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
+request(base_url, function (error, response, body) {
   if (error) throw error;
   if (response.statusCode == 200) {
     const characters = JSON.parse(body).characters;
     loadCharacters(characters);
   }
 });
-
 const loadCharacters = (charactersObj) => {
   if (charactersObj) {
     for (const character of charactersObj) {
