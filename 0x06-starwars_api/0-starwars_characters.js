cfc92a793806af5ae98613c271673d4560f77590
Eugene <140322568+yoojen@users.noreply.github.com>
2024-01-18 21:52:56 +0200

    Update 0-starwars_characters.js

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -5,7 +5,7 @@ const base_url = `https://swapi-api.alx-tools.com/api/films/${process.argv[2]}`;
 request(base_url, (error, response, body) => {
   if (error) throw error;
   if (response.statusCode == 200) {
-    characters = JSON.parse(body).characters;
+    const characters = JSON.parse(body).characters;
     loadCharacters(characters);
   }
 });
