80daef7df2c86f6bdf78c1ff500bda9c01512dde
yoojen <eugenemutuyimana15@gmail.com>
2024-01-19 14:57:18 +0000

    request one by one

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-const request = require("request");
+const request = require('request');
 
 const BaseUrl = `https://swapi-api.hbtn.io/api/films/${process.argv[2]}`;
 request(BaseUrl, function (error, response, body) {
@@ -10,8 +10,8 @@ request(BaseUrl, function (error, response, body) {
   }
 });
 const loadCharacters = (charactersObj, index) => {
-  if (index == charactersObj.length) return;
-  request(character[index], (error, response, body) => {
+  if (index === charactersObj.length) return;
+  request(charactersObj[index], (error, response, body) => {
     if (error) throw error;
     if (response.statusCode === 200) console.log(JSON.parse(body).name);
     loadCharacters(charactersObj, index + 1);
